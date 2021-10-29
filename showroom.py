fc7488b3fb8df43e9f424deac5073834fd850be4
Dani Pragustia <danipragustia@disroot.org>
2021-10-29 23:25:47 +0700

    update: make script exit instantly when ctrl+c

--- showroom.py
+++ showroom.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 from showroom.main import main
-
+import os
+import sys
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('[-] Interrupted, exiting program...')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
